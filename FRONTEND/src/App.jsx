ef601f48fa2c91b49f9b7d260d069f4fce2c121d
emmanuel onosode <peekweb1@gmail.com>
2024-12-31 05:01:42 +0100

    initialize React application with Vite setup and basic components

---
+++ FRONTEND/src/App.jsx
@@ -0,0 +1,18 @@
+import Nav from "./COMPONENT/nav"
+
+
+
+function App() {
+
+  
+  return (
+    
+    <div className="App">
+      <Nav />
+      <h1>HELLO WORLD</h1>
+      <button className="btn-primary"> BUTTON</button>
+    </div>
+  )
+}
+
+export default App
